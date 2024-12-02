bbd3af54484b7c6e1dabb5530b7435e3287cb91c
bsp3725k <139534037+bsp3725k@users.noreply.github.com>
2024-12-02 16:14:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-//your JS code here. If required.
+const form = document.getElementById("form");
+const nameInput = document.getElementById("name");
+const ageInput = document.getElementById("age");
+
+form.addEventListener("submit", (event) => {
+        event.preventDefault(); // Prevent form submission
+        const name = nameInput.value.trim();
+        const age = parseInt(ageInput.value, 10);
+
+        const agePromise = new Promise((resolve, reject) => {
+          setTimeout(() => {
+            if (age > 18) {
+              resolve(`Welcome, ${name}. You can vote.`);
+            } else {
+              reject(`Oh sorry ${name}. You aren't old enough.`);
+            }
+          }, 4000);
+        });
+
+        agePromise
+          .then((message) => {
+            alert(message);
+          })
+          .catch((error) => {
+            alert(error);
+          });
+      });
